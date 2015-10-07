66d05c97bced0c3d02a76db14b2cad90ed34b730
anukin <anukin2611@gmail.com>
2015-10-07 14:32:32 +0530

    Registration completed

--- registration/register.go
+++ registration/register.go
@@ -1,10 +1,25 @@
 package registration
 
+import (
+	"database/sql"
+	"fmt"
+)
+
 type user struct {
-	name     string
-	password string
+	name        string
+	password    string
+	transaction *sql.Tx
 }
 
 func (u user) Register() string {
-	return "Successfully registered"
+	var username string
+	err := u.transaction.QueryRow("SELECT * from users WHERE name=$1", u.name).Scan(&username)
+	switch {
+	case err == sql.ErrNoRows:
+		u.transaction.Query("INSERT INTO users(name, password) VALUES($1, $2)", u.name, u.password)
+		return "Successfully registered"
+	default:
+		fmt.Println(err)
+		return "User exists with same name.Please try a new username"
+	}
 }
--- registration/register_test.go
+++ registration/register_test.go
@@ -1,11 +1,23 @@
 package registration
 
 import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func TestRegistration_Register(t *testing.T) {
-	User := user{name: "anukin", password: "lol"}
-	assert.Equal(t, "Successfully registered", User.Register(), "User should be able to register successfully")
+func TestUser_Register(t *testing.T) {
+	db, _ := sql.Open("postgres", "user=CodeWalker dbname=twitchblade_test sslmode=disable")
+	tx, err := db.Begin()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	defer tx.Rollback()
+	defer db.Close()
+	User_1 := user{name: "bobo", password: "lol", transaction: tx}
+	assert.Equal(t, "Successfully registered", User_1.Register(), "User should be able to register successfully")
+	User_2 := user{name: "bobo", password: "lol", transaction: tx}
+	assert.Equal(t, "User exists with same name.Please try a new username", User_2.Register(), "User name should be unique")
 }
