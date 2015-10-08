12fabc56b3d48b065cfdf00e7a40afe7c58fa16a
anukin <anukin2611@gmail.com>
2015-10-08 14:26:19 +0530

    Added registration to twitchblade

--- registration/register_test.go
+++ registration/register_test.go
@@ -17,7 +17,7 @@ func TestUser_Register(t *testing.T) {
 	defer tx.Rollback()
 	defer db.Close()
 	User_1 := user{name: "bobo", password: "lol", transaction: tx}
-	assert.Equal(t, "Successfully registered", User_1.Register(), "User should be able to register successfully")
 	User_2 := user{name: "bobo", password: "lol", transaction: tx}
+	assert.Equal(t, "Successfully registered", User_1.Register(), "User should be able to register successfully")
 	assert.Equal(t, "User exists with same name.Please try a new username", User_2.Register(), "User name should be unique")
 }
