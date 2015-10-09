a4618aeae6901aa8bf4ba9affcc20ad982abd7e2
anukin <anukin2611@gmail.com>
2015-10-09 13:16:31 +0530

    Only registered users can access twitchblade

--- login/login.go
+++ login/login.go
@@ -1,7 +1,7 @@
 package login
 
 import (
-	_ "database/sql"
+	"database/sql"
 	_ "fmt"
 	"github.com/anukin/twitchblade/mylib"
 )
@@ -9,5 +9,12 @@ import (
 type User mylib.User
 
 func (u User) Login() string {
+	var username, password string
+	err := u.Transaction.QueryRow("Select * from users where name=$1", u.Name).Scan(&username, &password)
+	//fmt.Println(password)
+	switch {
+	case err == sql.ErrNoRows:
+		return "There is no user with that name, please try again!"
+	}
 	return "Welcome to Twitchblade"
 }
--- login/login_test.go
+++ login/login_test.go
@@ -15,3 +15,10 @@ func TestUser_Login(t *testing.T) {
 	User_1 := User{Name: "anugrah", Password: "megamind", Transaction: tx}
 	assert.Equal(t, "Welcome to Twitchblade", User_1.Login(), "User should be able to login to twitchblade")
 }
+
+func TestUser_Login_1(t *testing.T) {
+	db, _ := sql.Open("postgres", "user=CodeWalker dbname=twitchblade_test sslmode=disable")
+	tx, _ := db.Begin()
+	User_1 := User{Name: "bobo", Password: "superboy", Transaction: tx}
+	assert.Equal(t, "There is no user with that name, please try again!", User_1.Login(), "Only Valid user should be able to use twitchblade")
+}
