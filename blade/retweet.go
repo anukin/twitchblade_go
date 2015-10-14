01dc1bd2cefefcac163f7c82ecdef384dbc7abaf
anukin <anukin2611@gmail.com>
2015-10-14 13:58:30 +0530

    Retweet can only be used once

--- blade/retweet.go
+++ blade/retweet.go
@@ -1,10 +1,25 @@
 package blade
 
 import (
-	_ "database/sql"
-	_ "fmt"
+	"database/sql"
+	"fmt"
 )
 
-func (u *User) Retweet(int) string {
-	return "Successfully retweeted tweet by anugrah"
+func (u *User) alreadyretweeted(tweetid int) bool {
+	var id int
+	err := u.Transaction.QueryRow("SELECT id from retweets where original_tweet_id = $1 and retweeted_by = $2", tweetid, u.Name).Scan(&id)
+	return (err != sql.ErrNoRows)
+}
+
+func (u *User) Retweet(tweetid int) string {
+	//return "Successfully retweeted tweet by anugrah"
+	if u.alreadyretweeted(tweetid) {
+		return "You have already retweeted this tweet"
+	} else {
+		var msg, originaluser string
+		u.Transaction.QueryRow("Select username, tweet from tweets where id = $1", tweetid).Scan(&originaluser, &msg)
+		_, retweetid := u.Tweet(msg)
+		u.Transaction.Exec("INSERT INTO retweets(original_tweet_id, retweeted_by, retweet_tweet_id)) VALUES($1,$2,$3)", tweetid, u.Name, retweetid)
+		return fmt.Sprintf("Successfully retweeted tweet by %s", originaluser)
+	}
 }
--- blade/retweet_test.go
+++ blade/retweet_test.go
@@ -21,3 +21,18 @@ func TestUser_Retweet(t *testing.T) {
 	User_2 := User{Name: "red", Password: "charizard", Transaction: tx}
 	assert.Equal(t, "Successfully retweeted tweet by anugrah", User_2.Retweet(tweet_id), "User should be able to retweet")
 }
+
+func TestUser_Retweet_1(t *testing.T) {
+	db, _ := sql.Open("postgres", "user=CodeWalker dbname=twitchblade_test sslmode=disable")
+	tx, err := db.Begin()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	User_1 := User{Name: "anugrah", Password: "megamind", Transaction: tx}
+	_, tweet_id := User_1.Tweet("lol")
+	defer tx.Rollback()
+	defer db.Close()
+	User_2 := User{Name: "red", Password: "charizard", Transaction: tx}
+	User_2.Retweet(tweet_id)
+	assert.Equal(t, "You have already retweeted this tweet", User_2.Retweet(tweet_id), "User should be able to retweet")
+}
