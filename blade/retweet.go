b845254fa39c34788e7eef3f88b1e74f89baa7d6
anukin <anukin2611@gmail.com>
2015-10-15 11:40:08 +0530

    Retweet now tweets only the original tweet

--- blade/retweet.go
+++ blade/retweet.go
@@ -6,20 +6,32 @@ import (
 )
 
 func (u *User) alreadyretweeted(tweetid int) bool {
-	var id int
-	err := u.Transaction.QueryRow("SELECT id from retweets where original_tweet_id = $1 and retweeted_by = $2", tweetid, u.Name).Scan(&id)
+	var id string
+	err := u.Transaction.QueryRow("SELECT retweeted_by from retweets where original_tweet_id = $1 and retweeted_by = $2", tweetid, u.Name).Scan(&id)
 	return (err != sql.ErrNoRows)
 }
 
-func (u *User) Retweet(tweetid int) string {
+func (u *User) iteratedretweet(tweetid int) (bool, int) {
+	var id int
+	err := u.Transaction.QueryRow("SELECT original_tweet_id from retweets where retweet_tweet_id = $1 and retweeted_by = $2", tweetid, u.Name).Scan(&id)
+	return (err != sql.ErrNoRows), id
+}
+
+func (u *User) Retweet(tweetid int) (string, int) {
 	//return "Successfully retweeted tweet by anugrah"
 	if u.alreadyretweeted(tweetid) {
-		return "You have already retweeted this tweet"
+		//fmt.Println(u.Name)
+		return "You have already retweeted this tweet", tweetid
 	} else {
-		var msg, originaluser string
-		u.Transaction.QueryRow("Select username, tweet from tweets where id = $1", tweetid).Scan(&originaluser, &msg)
-		_, retweetid := u.Tweet(msg)
-		u.Transaction.Exec("INSERT INTO retweets(original_tweet_id, retweeted_by, retweet_tweet_id)) VALUES($1,$2,$3)", tweetid, u.Name, retweetid)
-		return fmt.Sprintf("Successfully retweeted tweet by %s", originaluser)
+		if flag, originalid := u.iteratedretweet(tweetid); flag == false {
+			var msg, originaluser string
+			u.Transaction.QueryRow("Select username, tweet from tweets where id = $1", tweetid).Scan(&originaluser, &msg)
+			_, retweetid := u.Tweet(msg)
+			var retweet_tweet_id int
+			u.Transaction.QueryRow("INSERT INTO retweets(original_tweet_id, retweeted_by, retweet_tweet_id)) VALUES($1,$2,$3) returning retweet_tweet_id", tweetid, u.Name, retweetid).Scan(&retweet_tweet_id)
+			return fmt.Sprintf("Successfully retweeted tweet by %s", originaluser), retweetid
+		} else {
+			return u.Retweet(originalid)
+		}
 	}
 }
--- blade/retweet_test.go
+++ blade/retweet_test.go
@@ -19,7 +19,8 @@ func TestUser_Retweet(t *testing.T) {
 	defer tx.Rollback()
 	defer db.Close()
 	User_2 := User{Name: "red", Password: "charizard", Transaction: tx}
-	assert.Equal(t, "Successfully retweeted tweet by anugrah", User_2.Retweet(tweet_id), "User should be able to retweet")
+	message, _ := User_2.Retweet(tweet_id)
+	assert.Equal(t, "Successfully retweeted tweet by anugrah", message, "User should be able to retweet")
 }
 
 func TestUser_Retweet_1(t *testing.T) {
@@ -34,5 +35,27 @@ func TestUser_Retweet_1(t *testing.T) {
 	defer db.Close()
 	User_2 := User{Name: "red", Password: "charizard", Transaction: tx}
 	User_2.Retweet(tweet_id)
-	assert.Equal(t, "You have already retweeted this tweet", User_2.Retweet(tweet_id), "User should be able to retweet")
+	message, _ := User_2.Retweet(tweet_id)
+	assert.Equal(t, "You have already retweeted this tweet", message, "User should be able to retweet the same tweet only once")
+}
+
+func TestUser_Retweet_2(t *testing.T) {
+	db, _ := sql.Open("postgres", "user=CodeWalker dbname=twitchblade_test sslmode=disable")
+	tx, err := db.Begin()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	User_1 := User{Name: "anugrah", Password: "megamind", Transaction: tx}
+	_, tweet_id := User_1.Tweet("lol")
+	//defer tx.Rollback()
+	tx.Commit()
+	defer db.Close()
+	User_2 := User{Name: "red", Password: "charizard", Transaction: tx}
+	_, retweet_id := User_2.Retweet(tweet_id)
+	tx.Commit()
+	trax, _ := db.Begin()
+	User_3 := User{Name: "lol", Password: "lol", Transaction: trax}
+	message, _ := User_3.Retweet(retweet_id)
+	tx.Rollback()
+	assert.Equal(t, "Successfully retweeted tweet by anugrah", message, "User should be able to retweet those tweets of original tweet")
 }
